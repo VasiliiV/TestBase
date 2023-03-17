ea62c28cfdefee6b8214e91f825ab4c98a3fb00f
VasiliiV <volgincoin@list.ru>
2023-03-17 10:13:37 +0300

    refack code datavase

--- my-app/pages/api/click.js
+++ my-app/pages/api/click.js
@@ -4,20 +4,12 @@ import { open } from 'sqlite';
 export default async function click(req, res) {
     if (req.method === 'POST') {
       const { name, age } = req.body;
-      const apiResponse = await fetch('http://127.0.0.1:80/api', {
-        method: 'POST',
-        body: JSON.stringify({ name, age }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      const text = await apiResponse.json();
       const db = await open({
         filename: './sqlite/parsetags.db',
         driver: sqlite3.Database,
       });
       await db.run(`CREATE TABLE IF NOT EXISTS tags (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age TEXT)`);
-      await db.run(`INSERT INTO tags (text) VALUES (?, ?)`, JSON.stringify(text));
+      await db.run(`INSERT INTO tags (text) VALUES (?, ?)`, name, age);
       await db.close();
       res.status(200).json({ data: 'Success' });
     } else {
