eb456b4aa5eeb91c114de4e804b86daa7c41f863
VasiliiV <volgincoin@list.ru>
2023-03-17 10:38:37 +0300

    refack code for database

--- my-app/pages/api/click.js
+++ my-app/pages/api/click.js
@@ -1,6 +1,7 @@
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 
+
 export default async function click(req, res) {
     if (req.method === 'POST') {
       const { name, age } = req.body;
@@ -9,10 +10,14 @@ export default async function click(req, res) {
         driver: sqlite3.Database,
       });
       await db.run(`CREATE TABLE IF NOT EXISTS tags (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age TEXT)`);
-      await db.run(`INSERT INTO tags (text) VALUES (?, ?)`, name, age);
+      await db.run(`INSERT INTO tags (name, age) VALUES (?, ?)`, name, age);
       await db.close();
-      res.status(200).json({ data: 'Success' });
+      res.status(200).json({ 
+        name: name,
+        age: age 
+      });
     } else {
       res.status(404).json({ message: 'Not found' });
     }
+    console.log(data)
   }
