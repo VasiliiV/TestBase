8d7fe2ba4e62a8d678f57539b86341f8b9b9d9a0
VasiliiV <volgincoin@list.ru>
2023-03-17 09:29:25 +0300

    add function word with database

--- my-app/pages/api/click.js
+++ my-app/pages/api/click.js
@@ -1,8 +1,24 @@
-export default function click(req, res) {
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+export default async function click(req, res) {
     if (req.method === 'POST') {
       const { yandex, google } = req.body;
-      // Обработка POST запроса здесь
-      // ...
+      const apiResponse = await fetch('http://example.com/api', {
+        method: 'POST',
+        body: JSON.stringify({ yandex, google }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const text = await apiResponse.json();
+      const db = await open({
+        filename: './sqlite/parsetags.db',
+        driver: sqlite3.Database,
+      });
+     // await db.run(`CREATE TABLE IF NOT EXISTS tags (id INTEGER PRIMARY KEY AUTOINCREMENT, data TEXT)`);
+      await db.run(`INSERT INTO tags (text) VALUES (?)`, JSON.stringify(text));
+      await db.close();
       res.status(200).json({ data: 'Success' });
     } else {
       res.status(404).json({ message: 'Not found' });
