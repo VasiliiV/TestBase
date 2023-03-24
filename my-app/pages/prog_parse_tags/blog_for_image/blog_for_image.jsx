99a06b8c0f5760d7617b98a9c6f752e383feb563
VasiliiV <volgincoin@list.ru>
2023-03-24 17:06:33 +0300

    refack div

--- my-app/pages/prog_parse_tags/blog_for_image/blog_for_image.jsx
+++ my-app/pages/prog_parse_tags/blog_for_image/blog_for_image.jsx
@@ -5,13 +5,13 @@ function BlogForImage() {
     return (
         <div className="Header-Right">
             <div className="Header-User">
-                <a className="user-account" href="" role={"button"}>
+                <div className="user-account" href="">
                     <span className="user-account_name"></span>
                     <div className="user-account_icon">
                         <img src="" alt="" sizes=""/>
                     </div>
                     <button className="Button_view_image" type="button">Загрузить</button>
-                </a>
+                </div>
             </div>
         </div>
     );
