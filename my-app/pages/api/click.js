a0380751624169dfa2fb32498abaeabbeaae68c4
VasiliiV <volgincoin@list.ru>
2023-03-31 13:08:14 +0300

    refact api get for button

--- my-app/pages/api/click.js
+++ my-app/pages/api/click.js
@@ -3,21 +3,32 @@ import { open } from 'sqlite';
 
 
 export default async function click(req, res) {
-    if (req.method === 'POST') {
-      const { name, age } = req.body;
-      const db = await open({
-        filename: './sqlite/parsetags.db',
-        driver: sqlite3.Database,
-      });
-      await db.run(`CREATE TABLE IF NOT EXISTS tags (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age TEXT)`);
-      await db.run(`INSERT INTO tags (name, age) VALUES (?, ?)`, name, age);
-        const lastRow = await db.get("SELECT name FROM tags ORDER BY id DESC LIMIT 1");
-      await db.close();
-        res.status(200).json({
-        name: lastRow.name,
-        age: age,
-      });
-    } else {
-      res.status(404).json({ message: "Not found" });
-      }
+  if (req.method === "POST") {
+    const { name, age } = req.body;
+    const db = await open({
+      filename: "./sqlite/parsetags.db",
+      driver: sqlite3.Database,
+    });
+    await db.run("INSERT INTO tags (name, age) VALUES (?, ?)", name, age);
+    await db.close();
+    res.status(200).json({
+      name: name,
+      age: age,
+    });
+  } else if (req.method === "GET") {
+    const db = await open({
+      filename: "./sqlite/parsetags.db",
+      driver: sqlite3.Database,
+    });
+    const lastRow = await db.get(
+      "SELECT name, age FROM tags ORDER BY id DESC LIMIT 1"
+    );
+    await db.close();
+    res.status(200).json({
+      name: lastRow.name,
+      age: lastRow.age,
+    });
+  } else {
+    res.status(404).json({ message: "Not found" });
+  }
 }
--- my-app/pages/prog_parse_tags/blog_for_image/SectionImage.jsx
+++ my-app/pages/prog_parse_tags/blog_for_image/SectionImage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
     const SectionImage = () => {
     const [userName, setUserName] = useState("");
     const handleClick = async () => {
-    const response = await fetch('/api/click', { method: 'POST' });
+    const response = await fetch('/api/click', { method: 'GET' });
     const data = await response.json();
     setUserName(data.name);
     };
@@ -10,7 +10,7 @@ return (
 <div className="headerRight">
     <div className="headerUser">
         <div className="user-account" href="">
-            <span className="user-account_name">{userName}</span> //тут хочу оторазить имя из бд
+            <span className="user-account_name">{userName}<p>Тут отобразится твоё имя</p></span>
             <div className="user-account_icon">
                 <img src="/multfilm_gomer.png" alt="" width={120} />
             </div>
