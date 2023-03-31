251ed8476bdf44316af7bf4cad04054ba857dbee
VasiliiV <volgincoin@list.ru>
2023-03-31 10:53:49 +0300

    refack image and add username from db

--- my-app/pages/prog_parse_tags/blog_for_image/SectionImage.jsx
+++ my-app/pages/prog_parse_tags/blog_for_image/SectionImage.jsx
@@ -1,20 +1,25 @@
-import React from "react";
-
-
-const SectionImage = () => {
-    return (
-        <div className="headerRight">
-            <div className="headerUser">
-                <div className="user-account" href="">
-                    <span className="user-account_name"></span>
-                    <div className="user-account_icon">
-                        <img src="/multfilm_gomer.png" alt="" width={120}/>
-                    </div>
-                    <button className="Button_view_button" type="button">Загрузить</button>
-                </div>
+import React, { useState } from "react";
+    const SectionImage = () => {
+    const [userName, setUserName] = useState("");
+    const handleClick = async () => {
+    const response = await fetch('/api/click', { method: 'POST' });
+    const data = await response.json();
+    setUserName(data.name);
+    };
+return (
+<div className="headerRight">
+    <div className="headerUser">
+        <div className="user-account" href="">
+            <span className="user-account_name">{userName}</span>
+            <div className="user-account_icon">
+                <img src="/multfilm_gomer.png" alt="" width={120} />
             </div>
+            <button className="Button_view_button" type="button" onClick={handleClick}>
+            Add userName
+            </button>
         </div>
-    );
-}
-
+    </div>
+</div>
+);
+};
 export default SectionImage;
