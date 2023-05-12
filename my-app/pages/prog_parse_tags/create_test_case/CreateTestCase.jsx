dd9d1e6cae4f7b94a5269c3dd5faea0dfd89d5a8
VasiliiV <volgincoin@list.ru>
2023-05-12 11:11:57 +0300

    refact test case

--- my-app/pages/prog_parse_tags/create_test_case/CreateTestCase.jsx
+++ my-app/pages/prog_parse_tags/create_test_case/CreateTestCase.jsx
@@ -3,8 +3,20 @@ import { useState } from "react";
 function CreateTestCase() {
 
     return (
-        <div>
-            
+        <div className="testCase">
+            <div className="testCase__header">
+                <h2>Test Case</h2>
+            </div>
+            <div className="testCase__content">
+                <div className="testCaseOverview">
+                    <div className="testCaseOverview__primary">
+
+                    </div>
+                    <div className="testCaseOverview__secondary">
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
