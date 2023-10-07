7fb19364785bbb39e2a65c00a6707b01d18160a7
Vasiliy Volgin <82467534+VasiliiV@users.noreply.github.com>
2023-10-07 13:39:04 +0300

    add test

--- testBaseAutoTest/src/test/java/testBase/BaseTest.java
+++ testBaseAutoTest/src/test/java/testBase/BaseTest.java
@@ -1,32 +1,32 @@
 package testBase;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.selector.ByText;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-@CloseWD
 public class BaseTest {
+
+    private static final String BASE_URL = "http://localhost:3000";
+
     @Test
-    public void openSite() {
-        open("http://localhost:3000");
-        $$x("//input[@type='checkbox']")
-                .forEach(element -> element.click());
-        $$x("//input[@type='text']")
-                .get(0)
-                .sendKeys("Vasiliy32");
-        $$x("//input[@type='text']")
-                .get(1)
-                .sendKeys("32");
-        $(byText("Сохранить"))
-                .click();
-        $(byText("Показать имя"))
-                .shouldBe(visible.because("Не отображается кнопка"))
-                .click();
-        sleep(1000);
+    public void openSiteAndPerformActions() {
+        open(BASE_URL);
+
+        // Отмечаем все чекбоксы
+        $$("input[type='checkbox']").forEach(SelenideElement::click);
+
+        // Вводим данные в текстовые поля
+        $$("input[type='text']").get(0).setValue("Vasiliy32");
+        $$("input[type='text']").get(1).setValue("32");
+
+        // Нажимаем кнопку сохранения
+        $("button:contains('Сохранить')").click();
+
+        // Проверяем наличие и нажимаем кнопку "Показать имя"
+        $("button:contains('Показать имя')").shouldBe(visible.because("Кнопка 'Показать имя' не отображается")).click();
     }
 }
+
 
