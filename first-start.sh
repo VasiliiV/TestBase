b984658d1a4d4abdf099e69d9c596c7b8ba5198e
Vasiliy Volgin <82467534+VasiliiV@users.noreply.github.com>
2024-04-29 09:51:26 +0300

    refact script

--- first-start.sh
+++ first-start.sh
@@ -1,18 +1,31 @@
-#! /bin/bash
+#!/bin/bash
 
 cd ./my-app
 
+# Удаление папки node_modules, если она существует
 if [ -d "node_modules" ]; then
-rm -rf node_modules; 
+    echo "Удаление существующей папки node_modules..."
+    rm -rf node_modules
 else 
-echo "Папка не найдена"
-
+    echo "Папка node_modules не найдена. Продолжение установки..."
 fi
-name=test
 
-echo "Установка зависимостей $name"
-npm i
+# Проверка наличия файла package.json перед установкой зависимостей
+if [ -f "package.json" ]; then
+    echo "Установка зависимостей..."
+    npm i
 
+    echo "Установка библиотеки для хеширования паролей и работы с JWT..."
+    npm install jsonwebtoken bcrypt
 
-echo "Запуск приложения"
-npm run dev
+    # Копирование .env.example в .env.local, если последний не существует
+    if [ ! -f ".env.local" ]; then
+        echo "Создание .env.local из .env.example..."
+        cp .env.example .env.local
+    fi
+
+    echo "Запуск приложения..."
+    npm run dev
+else
+    echo "Файл package.json не найден. Установка зависимостей невозможна."
+fi
